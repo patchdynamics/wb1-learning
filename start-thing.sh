3749e725a4c33f34316162b2918675a21936c616
Matthew Shultz <shultzm@stanford.edu>
2016-12-08 14:46:46 -0800

    Updated start script

--- start-thing.sh
+++ start-thing.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+ALG=$1
+MODE=$2
+mkdir -p ../$ALG
+rsync -r * ../$ALG
+cd ../$ALG
 rm slurm*
 ./scripts/clear.sh
-sbatch --job-name=THING --mail-type=END,FAIL  --mail-user=shultzm@stanford.edu  scripts/run.thing.cluster.sh
+sbatch --job-name=THING --mail-type=END,FAIL  --mail-user=shultzm@stanford.edu --time=300  scripts/run.$ALG.$MODE.cluster.sh
