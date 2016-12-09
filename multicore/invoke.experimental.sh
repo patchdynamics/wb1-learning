a6bc9532d2ea8e35f1e728c7c221e4adfd76778d
Matthew Shultz <shultzm@stanford.edu>
2016-12-09 15:09:48 -0800

    more multi

--- multicore/invoke.experimental.sh
+++ multicore/invoke.experimental.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 ALG=$1
-FLAG=$2
+ALGl="${ALG,,}"
+FLAG=$3
+EPSILON=$2
 MODE='experimental'
-cd /home/mshultz/learning/wb1-learning/multicore/$MODE.$ALG$FLAG$i
+TASK=$SLURM_ARRAY_TASK_ID
+cd /home/mshultz/learning/multicore/$MODE.$ALGl$FLAG$TASK
 module load Python/2.7.11
 export PYTHONPATH=$PYTHONPATH:/home/mshultz/virtualenvs/py2.7/lib/python2.7/
 python --version
 set -e
-TASK=$SLURM_ARRAY_TASK_ID
 if [ $TASK -eq 0 ]
 then
   python runSimulation.py -e 0 --dams 1 --year 2015 -a $ALG $FLAG --test #> /dev/null
 else
-  python runSimulation.py -e $epsilon --dams 1 --year 2015 -a $ALG $FLAG #> /dev/null
+  echo $EPSILON
+  python runSimulation.py -e $EPSILON --dams 1 --year 2015 -a $ALG $FLAG #> /dev/null
 fi
 
--- multicore/multi.sh
+++ multicore/multi.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 ALG=$1
-ALG="${ALG,,}"
+ALGl="${ALG,,}"
 MODE=$2
 FLAGS=$3
 PEERS=5
-#rm slurm*
+rm slurm*
 
 #setup
 for i in `seq 0 $PEERS`;
 do
-  mkdir -p ../multicore/$ALG.$MODE$FLAGS$i
-  rsync -r --links --exclude 'models' * ../multicore/$ALG.$MODE$FLAGS$i
+  mkdir -p ../multicore/$MODE.$ALGl$FLAGS$i
+  rsync -r --links --exclude 'models' * ../multicore/$MODE.$ALGl$FLAGS$i
 done  
 
 
 echo $INDEX > status.log
-RES=$(sbatch --array=1-$PEERS -p DGE scripts/multi/invoke.$MODE.sh $ALG $FLAGS)
+RES=$(sbatch --array=0-$PEERS -p DGE multicore/invoke.$MODE.sh $ALG '0.5' $FLAGS )
 #RES=$(sbatch --dependency=afterok:${RES##* } -p DGE combine.weights.sh)
 #$INDEX = $((INDEX + 1))
 #RES=$(sbatch --array=1-$PEERS -p DGE ./wait.sh)
