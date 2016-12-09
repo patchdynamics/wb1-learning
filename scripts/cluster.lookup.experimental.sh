84229db3bed1044a0cc06e91debdb302ca39e467
Matthew Shultz <shultzm@stanford.edu>
2016-12-09 10:49:49 -0800

    Scripts

---
+++ scripts/cluster.lookup.experimental.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+module load Python/2.7.11
+export PYTHONPATH=$PYTHONPATH:/home/mshultz/virtualenvs/py2.7/lib/python2.7/
+python --version
+anneal=(.5 .4 .4 .3 .3 .2 .2 .1 .1 .1 .05 .05 .05 .01 .01 .01 0 0 0)
+set -e
+for epsilon in "${anneal[@]}"
+do
+	for n in $(seq 25)
+	do
+		echo $n	
+		echo $epsilon
+		python runSimulation.py -e $epsilon --dams 1 --year 2005 -a Lookup $1 #> /dev/null
+	  #python runSimulation.py -e $epsilon --dams 1 --year 2006 -a Lookup $1 #> /dev/null
+		#python runSimulation.py -e $epsilon --dams 1 --year 2007 -a Lookup $1 #> /dev/null
+  done
+done
---
+++ start-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ALG=$1
+MODE=$2
+FLAGS=$3
+mkdir -p ../$ALG.$MODE$FLAGS
+rsync -r --links * ../$ALG.$MODE$FLAGS
+cd ../$ALG.$MODE$FLAGS
+rm slurm*
+sbatch --job-name=$ALG$MODE --mail-type=END,FAIL  --mail-user=shultzm@stanford.edu -p DGE  scripts/cluster.$ALG.$MODE.sh "$FLAGS --test"
